fe131d6597b48cde1790b8c2827936a6fe62e79e
Terry Wang <terry.wang@accenture.com>
2019-03-04 23:10:22 -0500

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Yi Wang on 3/4/19.
+//  Copyright © 2019 Yi Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
